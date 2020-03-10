85097f1795b1bf12321dab717d7c9f098c0b5e76
hanum-lee <chococloudy@gmail.com>
2020-03-09 19:02:58 -0600

    Animation and color changing of the cube semi- working

--- Assets/Scripts/PlayPause.cs
+++ Assets/Scripts/PlayPause.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 
@@ -12,11 +13,21 @@ public class PlayPause : MonoBehaviour
     public GameObject object1;
     public Renderer rende;
     public Material matPlay;
+    public Material matPause;
 
+    public UnityEvent testing1B;
+    public UnityEvent testing2T;
+
+    public Animator cubeAnimator;
+    bool playing;
 
     void Start()
     {
         matPlay = Resources.Load<Material>("Materials/CubeMatPlay");
+        matPause = Resources.Load<Material>("Materials/CubeMatPause");
+        playing = false;
+        
+        
     }
 
     // Update is called once per frame
@@ -36,8 +47,31 @@ public class PlayPause : MonoBehaviour
         if (Physics.Raycast(ray, out hit) && clicked)
         {
             Debug.Log("Clicked on gameobject: " + hit.collider.name);
-            //rende.material = matPlay;
-            object1.GetComponent<MeshRenderer>().material = matPlay;
+
+            
+
+            if (playing)
+            {
+                //testing1B.Invoke();
+                //cubeAnimator.SetBool("Playing", true);
+                cubeAnimator.enabled = true;
+                cubeAnimator.Play("Bouncing");
+                playing = false;
+                object1.GetComponent<MeshRenderer>().material = matPause;
+                cubeAnimator.SetTrigger("isPlaying");
+            }
+            else
+            {
+                //testing2T.Invoke();
+                //cubeAnimator.SetBool("Playing", false);
+                cubeAnimator.enabled = false;
+                playing = true;
+                object1.GetComponent<MeshRenderer>().material = matPlay;
+                cubeAnimator.ResetTrigger("isPlaying");
+            }
+
+            
+            
         }
 
        
