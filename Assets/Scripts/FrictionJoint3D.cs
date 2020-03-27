3502a0afceb5326968607d547e62b5efd5813ee8
hanum-lee <chococloudy@gmail.com>
2020-03-27 16:36:31 -0600

    rope model and script added

---
+++ Assets/Scripts/FrictionJoint3D.cs
@@ -0,0 +1,25 @@
+﻿// Tutorial from https://www.youtube.com/watch?v=Cecp4a7FsTU&feature=emb_title
+using UnityEngine;
+
+public class FrictionJoint3D : MonoBehaviour
+{
+
+    [Range(0, 1)]
+    public float Friction;
+
+    protected Rigidbody Rigidbody;
+
+    void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody.velocity = Rigidbody.velocity * (1 - Friction);
+        Rigidbody.angularVelocity = Rigidbody.angularVelocity * (1 - Friction);
+    }
+
+
+
+}
