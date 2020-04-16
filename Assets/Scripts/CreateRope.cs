8b5fd0874faa472ed579b1a33b29ba9c85322dd6
hanum-lee <chococloudy@gmail.com>
2020-04-16 12:09:15 -0600

    added pitch changer

--- Assets/Scripts/CreateRope.cs
+++ Assets/Scripts/CreateRope.cs
@@ -5,17 +5,64 @@ using UnityEngine;
 public class CreateRope : MonoBehaviour
 {
     // Start is called before the first frame update
-    
 
-    void Start()
+    private float mouseY;
+    bool initalrelease;
+    float initialspeed;
+    Rigidbody rb;
+    float testing = .1f;
+
+    Transform[] childrenObject = new Transform[4];
+    List<Transform> childrenTrans;
+    List<GameObject> childGameObject;
+    AudioManager audioManager;
+
+    void Awake()
     {
-       
-        
+        rb = gameObject.GetComponent<Rigidbody>();
+        audioManager = FindObjectOfType<AudioManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (initalrelease)
+        {
+            var modspeed = initialspeed;
+            Debug.Log("Speed: " + modspeed);
+            initalrelease = false;
+        }
         
+    }
+
+    void OnMouseDrag()
+    {
+        mouseY = Input.GetAxis("Mouse Y");
+        
+        //transform.position = new Vector3(rb.position.x, Input.mousePosition.y, rb.position.z);
+        //transform.Translate(transform.position.x, mouseY, transform.position.z, Space.World);
+        rb.AddForce(mouseY * testing * Vector3.up );
+        
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log(mouseY);
+        var velocity = rb.velocity;
+        Debug.Log("Velocity:" + velocity);
+        initialspeed = velocity.magnitude;
+        if(mouseY > 0)
+        {
+            Debug.Log("Pitch UP");
+            audioManager.increasePitch();
+            audioManager.updateSound();
+        }
+        else
+        {
+            Debug.Log("Pitch DOWN");
+            audioManager.decreasePitch();
+            audioManager.updateSound();
+        }
+        initalrelease = true;
     }
 }
